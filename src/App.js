bab23c1303cad57dcce3da01a01a99ff07c2b7ce
Xiaochen Yang <xiaochey@usc.edu>
2020-03-30 00:49:34 -0700

    class 2 part2

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component{
       repos: [],
       members: [],
       loading: false,
+      searchValue: ''
     };
 
     this.handleClickReposButton = this.fetchRepos.bind(this);
@@ -38,9 +39,36 @@ class App extends React.Component{
     this.setState({members: members, loading: false, repos:[]});
   }
 
+  handleSearchInputChange = (event)=>{
+    this.setState({
+      searchValue: event.target.value
+    });
+  }
+
+  handleSearch = async (event)=>{
+    event.preventDefault();
+    console.log(this.state.searchValue);
+    this.setState({loading: true});
+
+    let [members, repos] = await Promise.all([
+      getMembers(this.state.searchValue),
+      getRepos(this.state.searchValue)
+    ]);
+
+    this.setState({members: members, repos: repos, loading: false});
+  }
+
   render(){
     return (
       <div>
+        <form onSubmit={this.handleSearch}>
+          <input 
+              type="text" 
+              value={this.state.searchValue}
+              onChange={this.handleSearchInputChange}
+          />
+          <button type="submit">Search</button>
+        </form>
          {/* {this.state.loading ? <Loading />: ""} */}
          {this.state.loading && <Loading />}
          <button onClick={this.handleClickReposButton}>Repos</button>
