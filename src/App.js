966e238916a4824bd7533f095754a87e4d293239
Xiaochen Yang <xiaochey@usc.edu>
2020-03-30 13:56:20 -0700

    class 2 part 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Loading from './Loading';
 import { getMembers, getRepos } from './GitHubApi';
 import GitHubReposList from './GitHubReposList';
 import GitHubMembersList from './GitHubMembersList';
+import SearchForm from './SearchForm';
 
 class App extends React.Component{
   constructor(props){
@@ -12,47 +13,15 @@ class App extends React.Component{
       repos: [],
       members: [],
       loading: false,
-      searchValue: ''
     };
-
-    this.handleClickReposButton = this.fetchRepos.bind(this);
-    this.handleClickMembersButton = this.fetchMembers.bind(this);
   }
   //methods
-  
-  //a syntax to remember 'this', experimental
-  // fetchRepos = async () => {
-  //   this.setState({loading: true});
-  //   let repos = await getRepos('emberjs');
-  //   this.setState({repos: repos, loading: false});
-  // }
-
-  async fetchRepos() {
-    this.setState({loading: true});
-    let repos = await getRepos('emberjs');
-    this.setState({repos: repos, loading: false, members:[]});
-  }
-
-  async fetchMembers(){
-    this.setState({loading: true});
-    let members = await getMembers('emberjs');
-    this.setState({members: members, loading: false, repos:[]});
-  }
-
-  handleSearchInputChange = (event)=>{
-    this.setState({
-      searchValue: event.target.value
-    });
-  }
-
-  handleSearch = async (event)=>{
-    event.preventDefault();
-    console.log(this.state.searchValue);
+  handleSearch = async (searchValue)=>{
     this.setState({loading: true});
 
     let [members, repos] = await Promise.all([
-      getMembers(this.state.searchValue),
-      getRepos(this.state.searchValue)
+      getMembers(searchValue),
+      getRepos(searchValue)
     ]);
 
     this.setState({members: members, repos: repos, loading: false});
@@ -61,18 +30,8 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        <form onSubmit={this.handleSearch}>
-          <input 
-              type="text" 
-              value={this.state.searchValue}
-              onChange={this.handleSearchInputChange}
-          />
-          <button type="submit">Search</button>
-        </form>
-         {/* {this.state.loading ? <Loading />: ""} */}
+        <SearchForm onSearch={this.handleSearch}/>
          {this.state.loading && <Loading />}
-         <button onClick={this.handleClickReposButton}>Repos</button>
-         <button onClick={this.handleClickMembersButton}>Members</button>
         <div>
 
         </div>
